8757f7566063995a0b1c04fd85b5eca556fd5f66
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-10-19 23:53:24 +0800

    style: customize colors (dracula theme)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      background: "#282A36",
+      current: "#44475A",
+      foreground: "#F8F8F2",
+      comment: "#6272A4",
+      cyan: "#8BE9FD",
+      green: "#50FA7B",
+      orange: "#FFB86C",
+      pink: "#FF79C6",
+      purple: "#BD93F9",
+      red: "#FF5555",
+      yellow: "#F1FA8C",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
