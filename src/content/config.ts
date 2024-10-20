ebaba05a46a1f216b738e4cba6d41e7b12962d9b
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-10-20 15:40:25 +0800

    fix: collection config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,14 @@
 import { z, defineCollection } from 'astro:content';
-const postsCollection = defineCollection({});
+const postsCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        tags: z.array(z.string()),
+        categories: z.array(z.string()).optional(),
+        abbrlink: z.string().optional(),
+        date: z.date(),
+    }),
+});
 export const collections = {
     'posts': postsCollection,
 };
