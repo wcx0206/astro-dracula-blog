c9af47c60d72cb96275a0c52eb1200dc07533bf0
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-10-22 08:17:28 +0800

    build(deps): add tailwind dracula theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,24 +4,13 @@ import defaultTheme from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    colors: {
-      background: "#282A36",
-      current: "#44475A",
-      foreground: "#F8F8F2",
-      comment: "#6272A4",
-      cyan: "#8BE9FD",
-      green: "#50FA7B",
-      orange: "#FFB86C",
-      pink: "#FF79C6",
-      purple: "#BD93F9",
-      red: "#FF5555",
-      yellow: "#F1FA8C",
-    },
     extend: {
       fontFamily: {
         code: ["Fira Code", ...defaultTheme.fontFamily.mono],
       }
     }
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-dracula')(),
+  ],
 };
