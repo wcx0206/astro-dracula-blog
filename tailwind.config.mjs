25b82a6b5ea988fc4e5fd8fbcb6e9c734d3e05ea
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-10-22 11:05:44 +0800

    refactor: use tailwind typo & dracula theme library

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,11 +7,33 @@ export default {
     extend: {
       fontFamily: {
         code: ["Fira Code", ...defaultTheme.fontFamily.mono],
-      }
+      },
+      typography: ({ theme }) => ({
+        dracula: {
+          css: {
+            '--tw-prose-body': theme('colors.dracula-light.DEFAULT'),
+            '--tw-prose-headings': theme('colors.dracula-pink.DEFAULT'),
+            '--tw-prose-lead': theme('colors.dracula-pink.DEFAULT'),
+            '--tw-prose-links': theme('colors.dracula-purple.DEFAULT'),
+            '--tw-prose-bold': theme('colors.dracula-purple.DEFAULT'),
+            '--tw-prose-counters': theme('colors.dracula-pink.DEFAULT'),
+            '--tw-prose-bullets': theme('colors.dracula-pink.DEFAULT'),
+            '--tw-prose-hr': theme('colors.dracula-pink.DEFAULT'),
+            '--tw-prose-quotes': theme('colors.dracula-light.DEFAULT'),
+            '--tw-prose-quote-borders': theme('colors.dracula-pink.DEFAULT'),
+            '--tw-prose-captions': theme('colors.dracula-pink.DEFAULT'),
+            '--tw-prose-code': theme('colors.dracula-purple.DEFAULT'),
+            // '--tw-prose-pre-code': theme(''),
+            // '--tw-prose-pre-bg': theme(''),
+            '--tw-prose-th-borders': theme('colors.dracula-dark.DEFAULT'),
+            '--tw-prose-td-borders': theme('colors.dracula-dark.DEFAULT'),
+          },
+        },
+      }),
     }
   },
   plugins: [
-    require('@tailwindcss/typography'),
     require('tailwind-dracula')('dracula'),
+    require('@tailwindcss/typography'),
   ],
 };
