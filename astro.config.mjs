71597afe44bc6c564670495fdc1c51127bc2909d
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-10-22 14:50:33 +0800

    build(deps): add @astropub/md

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import markdownIntegration from '@astropub/md'
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,5 +16,5 @@ export default defineConfig({
       }
     },
   },
-  integrations: [tailwind()]
+  integrations: [tailwind(), markdownIntegration()]
 });
