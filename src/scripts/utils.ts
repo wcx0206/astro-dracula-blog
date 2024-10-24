bf11085e741813160afb17b50badc7efd374c57a
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-10-24 16:07:03 +0800

    feat: tags with count

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -1,3 +1,14 @@
-export const uniqueLowerCaseTags = (tags: string[]) =>
-    [...new Set(tags.map((tag) => tag.toLowerCase()))];
+/**
+ * Returns a Map of tags with their lowercase versions as keys and counts as values.
+ * @param tags - An array of tags.
+ * @returns A Map of tags with their lowercase versions as keys and counts as values.
+ */
+export const uniqueLowerCaseTags = (tags: string[]): Map<string, number> => {
+  const tagCounts = new Map<string, number>();
+    tags.forEach(tag => {
+        const lowercaseTag = tag.toLowerCase();
+        tagCounts.set(lowercaseTag, (tagCounts.get(lowercaseTag) || 0) + 1);
+    });
+    return tagCounts;
+};
 
