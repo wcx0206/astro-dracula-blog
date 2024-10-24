ec782aa5802ec9b0929fec1ec50fb0e3e9c12c84
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-10-24 18:48:09 +0800

    refactor: label tag component

---
+++ src/components/label-tag.tsx
@@ -0,0 +1,17 @@
+export default function LabelTag(
+    { label, count = 1, type = "tag" }:
+        {
+            label: string,
+            count: number,
+            type: "tag" | "link"
+        }
+) {
+    const text = count > 1 ? `${label} (${count})` : label;
+    const href = type === "link" ? `/tags/${label}` : undefined;
+    return (
+        <a href={href}
+            class="inline-block px-2 py-1 text-dracula-purple bg-dracula-dark/80 hover:bg-dracula-dark transition">
+            {text}
+        </a>
+    );
+}
