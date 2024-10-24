c53259ecb749c94061bfcb059e27099b82a0b60a
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-10-24 18:58:41 +0800

    feat: use the label tag component

--- src/components/label-tag.tsx
+++ src/components/label-tag.tsx
@@ -7,13 +7,11 @@ export default function LabelTag(
         }
 ) {
     const text = count > 1 ? `${label} (${count})` : label;
-    const href = type === "link" ? `/tags/${label}` : undefined;
-    return (
-        <a href={href}
-            class="inline-block px-2 py-1 font-code
+    const tagComponent = (<code
+        class="inline-block px-2 py-1
                 text-dracula-purple bg-dracula-dark/80 
                 hover:bg-dracula-dark transition">
-            {text}
-        </a>
-    );
+        {text}
+    </code>);
+    return type === "link" ? <a href={`/tags/${label}`}>{tagComponent}</a> : tagComponent;
 }
