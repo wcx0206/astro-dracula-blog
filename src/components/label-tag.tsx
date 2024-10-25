8ffdc0b36ddf1ef880755d3e62ac83d7c3a59af4
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-10-25 12:10:35 +0800

    feat: use label tag component on the tag page

--- src/components/label-tag.tsx
+++ src/components/label-tag.tsx
@@ -1,16 +1,18 @@
 export default function LabelTag(
-    { label, count = 1, type = "tag" }:
+    { label, count = 1, type = "tag", size = "normal" }:
         {
             label: string,
             count?: number,
-            type?: "tag" | "link"
+            type?: "tag" | "link",
+            size?: "normal" | "large"
         }
 ) {
     const text = count > 1 ? `${label} (${count})` : label;
+    const className = size === "large" ? "text-4xl px-4 py-2" : "px-2 py-1";
     const tagComponent = (<code
-        className="inline-block px-2 py-1
+        className={`inline-block ${className}
                 text-dracula-purple bg-dracula-dark/80 
-                hover:bg-dracula-dark transition">
+                hover:bg-dracula-dark transition`}>
         {text}
     </code>);
     return type === "link" ? <a href={`/tags/${label}`}>{tagComponent}</a> : tagComponent;
