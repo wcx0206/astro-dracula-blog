5d6e97eaca4f651af591bb34b84e8fbcd34cf819
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-11-19 12:02:42 +0800

    build: optimize the new-post utility

--- scripts/new.js
+++ scripts/new.js
@@ -1,5 +1,17 @@
 /**
  * Create a new post in the content directory
+ *
+ * Usage: pnpm new [options] <post-title>
+ *
+ * Options:
+ *   -l, --lang <en|zh>   Set the language (default: en)
+ *   -d, --draft          Create a draft post (default: false)
+ *   -m, --mdx            Use MDX format (default: false)
+ *   -h, --help           Show this help message
+ *
+ * Example:
+ *   pnpm new "Hello World"
+ *   pnpm new -l zh "你好，世界"
  */
 
 import fs from "node:fs";
@@ -17,6 +29,14 @@ function getDate() {
 	const seconds = String(date.getSeconds()).padStart(2, "0");
 
 	return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+function getPostSlug(postTitle) {
+	let slug = slugify(postTitle).toLocaleLowerCase();
+	if (slug === "") {
+		slug = "untitled";
+	}
+	return slug;
 }
 
 const HELP_INFO = `Usage: pnpm new [options] <post-title>
@@ -54,16 +74,14 @@ if (args.help) {
 	process.exit(0);
 }
 
-const postTitle = args._[0];
-if (!postTitle) {
-	console.error("ERROR: Post name is required");
-	console.error(HELP_INFO);
-	process.exit(1);
+let postTitle = args._.join(" "); // Join the rest of the arguments
+if (!postTitle || postTitle.trim() === "") {
+	postTitle = "Untitled";
 }
 console.log("Creating new post:", postTitle);
 
 const fileExtension = args.mdx ? ".mdx" : ".md";
-const fileName = slugify(args[0]) + fileExtension;
+const fileName = getPostSlug(postTitle) + fileExtension;
 const fileTypeDir = args.draft ? "drafts" : "posts";
 const fileLangDir = args.lang;
 const fullPath = path.join(TARGET_DIR, fileTypeDir, fileLangDir, fileName);
@@ -76,9 +94,20 @@ if (fs.existsSync(fullPath)) {
 
 const content = `---
 title: ${postTitle}
+tags:
+  - tag1
+  - tag2
+  - tag3
 date: ${getDate()}
+updated: ${getDate()}
 ---
+
+Write your description here.
+
+<!--more-->
+
+Write your content here.
 `;
 
 fs.writeFileSync(fullPath, content);
-console.log(`Post ${postTitle} created`);
+console.log(`Post "${postTitle}" created`);
