95565d39422ee1ff8d7538ab4e6d04bb4c12ee86
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-11-20 15:07:13 +0800

    feat: mermaid support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import remarkMath from 'remark-math';
-import rehypeMathjax from 'rehype-mathjax';
-import rehypeExternalLinks from 'rehype-external-links';
-import { rehypeGithubAlerts } from 'rehype-github-alerts';
+import { defineConfig } from "astro/config";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeMathjax from "rehype-mathjax";
+import remarkMath from "remark-math";
+import remarkMermaid from "remark-mermaidjs";
+import { rehypeGithubAlerts } from "rehype-github-alerts";
 
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 
-import { SITE } from './src/config.ts';
+import { SITE } from "./src/config.ts";
 import { remarkDescPlugin } from "./src/utils/markdown.ts";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.url,
-  markdown: {
-    shikiConfig: {
-      theme: "dracula",
-      wrap: true,
-      langAlias: {
-        "C": "c",
-        "zshrc": "zsh",
-      }
-    },
-    remarkPlugins: [remarkDescPlugin, remarkMath],
-    rehypePlugins: [rehypeMathjax, rehypeExternalLinks, rehypeGithubAlerts],
-  },
-  integrations: [react(), sitemap(), tailwind(), partytown()],
-  output: "static"
+	site: SITE.url,
+	markdown: {
+		shikiConfig: {
+			theme: "dracula",
+			wrap: true,
+			langAlias: {
+				C: "c",
+				zshrc: "zsh",
+			},
+		},
+		remarkPlugins: [remarkDescPlugin, remarkMath, remarkMermaid],
+		rehypePlugins: [rehypeExternalLinks, rehypeGithubAlerts, rehypeMathjax],
+	},
+	integrations: [react(), sitemap(), tailwind(), partytown()],
+	output: "static",
 });
