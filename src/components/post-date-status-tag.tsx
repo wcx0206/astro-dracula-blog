0b4adbd160aa83d4cd4c3b35a977c11781045b47
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-11-21 12:35:30 +0800

    fix: color issue

--- src/components/post-date-status-tag.tsx
+++ src/components/post-date-status-tag.tsx
@@ -4,12 +4,17 @@ import { type Lang, useTranslations } from "@/utils/i18n";
 export default function PostDateStatusTag({ lang, diffInDays }: { lang: Lang; diffInDays: number }) {
   const t = useTranslations(lang);
   const color = getColor(diffInDays);
-  const textColor = `text-dracula-${color}`;
 
   if (color === "orange") return null;
+  // `text-dracula-green` and `text-dracula-red` must be in the form of literals,
+  // otherwise the associated classes will not be packaged
   return (
-    <code className={`inline-block bg-dracula-dark/30 px-2 py-1 ${textColor}`}>
-      {color === "green" ? t("post.newlyUpdatedMsg") : color === "red" ? t("post.oldPostWarningMsg") : undefined}
+    <code className="inline-block bg-dracula-dark/30 px-2 py-1">
+      {color === "green" ? (
+        <span className="text-dracula-green">{t("post.newlyUpdatedMsg")}</span>
+      ) : color === "red" ? (
+        <span className="text-dracula-red">{t("post.oldPostWarningMsg")}</span>
+      ) : undefined}
     </code>
   );
 }
