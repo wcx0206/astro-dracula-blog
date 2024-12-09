d44443b189f2dbc87e0d6ad53e12c440da87e33b
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-12-09 16:35:16 +0800

    fix: reduce go-top button animation

--- src/components/ui/go-top.tsx
+++ src/components/ui/go-top.tsx
@@ -6,7 +6,6 @@ import { motion, useReducedMotion, AnimatePresence } from "motion/react";
 export default function GoTop() {
   const shouldReduceMotion = useReducedMotion();
   const initialOpacity = shouldReduceMotion ? 1 : 0;
-  const initialY = shouldReduceMotion ? 0 : 10;
 
   const [showGoTop, setShowGoTop] = useState(false);
   const handleVisibleButton = () => {
@@ -26,11 +25,10 @@ export default function GoTop() {
     <AnimatePresence>
       {showGoTop && (
         <motion.button
-          initial={{ opacity: initialOpacity, y: initialY }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: initialOpacity, y: initialY }}
+          initial={{ opacity: initialOpacity }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: initialOpacity }}
           whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
           className="size-8 flex items-center justify-center fixed bottom-5 right-5
         bg-dracula-dark/20 hover:bg-dracula-dark transition"
           onClick={handleScrollUp}
