500b34e500c09e0a2c2868d5b3e79dae68d091c1
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-12-09 16:26:07 +0800

    feat: animation for go-top button

--- src/components/ui/go-top.tsx
+++ src/components/ui/go-top.tsx
@@ -1,8 +1,13 @@
 // https://dev.to/silviaespanagil/how-to-create-a-scroll-to-top-button-with-react-17do
 import UpIcon from "@/components/icons/up";
 import { useState, useEffect } from "react";
+import { motion, useReducedMotion, AnimatePresence } from "motion/react";
 
 export default function GoTop() {
+  const shouldReduceMotion = useReducedMotion();
+  const initialOpacity = shouldReduceMotion ? 1 : 0;
+  const initialY = shouldReduceMotion ? 0 : 10;
+
   const [showGoTop, setShowGoTop] = useState(false);
   const handleVisibleButton = () => {
     setShowGoTop(window.scrollY > 100);
@@ -18,14 +23,21 @@ export default function GoTop() {
   }, []);
 
   return (
-    showGoTop && (
-      <button
-        className="size-8 flex items-center justify-center fixed bottom-5 right-5
+    <AnimatePresence>
+      {showGoTop && (
+        <motion.button
+          initial={{ opacity: initialOpacity, y: initialY }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: initialOpacity, y: initialY }}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="size-8 flex items-center justify-center fixed bottom-5 right-5
         bg-dracula-dark/20 hover:bg-dracula-dark transition"
-        onClick={handleScrollUp}
-      >
-        <UpIcon />
-      </button>
-    )
+          onClick={handleScrollUp}
+        >
+          <UpIcon />
+        </motion.button>
+      )}
+    </AnimatePresence>
   );
 }
