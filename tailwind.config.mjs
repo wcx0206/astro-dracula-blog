b13b082fea5787867c26844a7c157eb643d1961f
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-12-11 23:59:21 +0800

    feat: noto font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,34 +6,33 @@ export default {
   theme: {
     extend: {
       fontFamily: {
+        sans: ["Noto Sans SC", ...defaultTheme.fontFamily.sans],
         code: ["Fira Code", ...defaultTheme.fontFamily.mono],
       },
       typography: ({ theme }) => ({
         dracula: {
           css: {
-            '--tw-prose-body': theme('colors.dracula-light.DEFAULT'),
-            '--tw-prose-headings': theme('colors.dracula-pink.DEFAULT'),
-            '--tw-prose-lead': theme('colors.dracula-pink.DEFAULT'),
-            '--tw-prose-links': theme('colors.dracula-cyan.DEFAULT'),
-            '--tw-prose-bold': theme('colors.dracula-purple.DEFAULT'),
-            '--tw-prose-counters': theme('colors.dracula-pink.DEFAULT'),
-            '--tw-prose-bullets': theme('colors.dracula-pink.DEFAULT'),
-            '--tw-prose-hr': theme('colors.dracula-pink.DEFAULT'),
-            '--tw-prose-quotes': theme('colors.dracula-light.DEFAULT'),
-            '--tw-prose-quote-borders': theme('colors.dracula-pink.DEFAULT'),
-            '--tw-prose-captions': theme('colors.dracula-pink.DEFAULT'),
-            '--tw-prose-code': theme('colors.dracula-purple.DEFAULT'),
-            // '--tw-prose-pre-code': theme(''),
-            // '--tw-prose-pre-bg': theme(''),
-            '--tw-prose-th-borders': theme('colors.dracula-dark.DEFAULT'),
-            '--tw-prose-td-borders': theme('colors.dracula-dark.DEFAULT'),
+            "--tw-prose-body": theme("colors.dracula-light.DEFAULT"),
+            "--tw-prose-headings": theme("colors.dracula-pink.DEFAULT"),
+            "--tw-prose-lead": theme("colors.dracula-pink.DEFAULT"),
+            "--tw-prose-links": theme("colors.dracula-cyan.DEFAULT"),
+            "--tw-prose-bold": theme("colors.dracula-purple.DEFAULT"),
+            "--tw-prose-counters": theme("colors.dracula-pink.DEFAULT"),
+            "--tw-prose-bullets": theme("colors.dracula-pink.DEFAULT"),
+            "--tw-prose-hr": theme("colors.dracula-pink.DEFAULT"),
+            "--tw-prose-quotes": theme("colors.dracula-light.DEFAULT"),
+            "--tw-prose-quote-borders": theme("colors.dracula-pink.DEFAULT"),
+            "--tw-prose-captions": theme("colors.dracula-pink.DEFAULT"),
+            "--tw-prose-code": theme("colors.dracula-purple.DEFAULT"),
+            "--tw-prose-th-borders": theme("colors.dracula-dark.DEFAULT"),
+            "--tw-prose-td-borders": theme("colors.dracula-dark.DEFAULT"),
           },
         },
       }),
-    }
+    },
   },
   plugins: [
-    require('tailwind-dracula')('dracula'),
-    require('@tailwindcss/typography'),
+    require("tailwind-dracula")("dracula"),
+    require("@tailwindcss/typography"),
   ],
 };
